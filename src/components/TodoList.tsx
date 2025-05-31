c5b2e416f00ad74c9253a2493b022d29e66cb0ad
Anmol Shrestha <nmol.crestha@gmail.com>
2025-05-31 17:13:02 +0545

    Added popover calender

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,75 +1,101 @@
-import React from 'react'
-import { ScrollArea } from './ui/scroll-area';
-import { Card } from './ui/card';
-import { Checkbox } from './ui/checkbox';
+"use client";
+
+import React, { useState } from "react";
+import { ScrollArea } from "./ui/scroll-area";
+import { Card } from "./ui/card";
+import { Checkbox } from "./ui/checkbox";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import { Calendar1Icon } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar } from "./ui/calendar";
 
 const TodoList = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      Calender
+      <h1 className="text-lg font-medium mb-6">Todo List</h1>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button className="w-full">
+            <Calendar1Icon />
+            {date ? format(date, "PPP") : <span>Pick A date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 w-auto">
+          <Calendar
+          mode="single"
+          selected={date}
+          onSelect={ date => {
+            setDate(date);
+            setOpen(false); 
+          }}
+          />
+        </PopoverContent>
+      </Popover>
       <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
-        <div className='flex flex-col gap-4'>
-
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <div className="flex flex-col gap-4">
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
-        <Card className='p-4'>
-          <div className='flex items-center gap-4'>
-            <Checkbox id="item1"/>
-            <label htmlFor="item1" className="text-sm text-muted-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </label>
-          </div>
-        </Card>
+            </div>
+          </Card>
         </div>
       </ScrollArea>
     </div>
   );
-}
+};
 
-export default TodoList
+export default TodoList;
