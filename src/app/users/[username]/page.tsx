b2fa65b70ac8c29c61a0582c2012863e8d2843b9
Anmol Shrestha <nmol.crestha@gmail.com>
2025-06-03 20:29:37 +0545

    Added sheet component

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -15,7 +15,10 @@ import {
 } from "@/components/ui/hover-card";
 import { Progress } from "@/components/ui/progress";
 import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import EditUser from "@/components/EditUser";
 
 const SingleUser = () => {
   return (
@@ -104,7 +107,15 @@ const SingleUser = () => {
           </div>
           {/* INFORMATION CONTAINER */}
           <div className="bg-primary-foreground p-4 rounded-lg">
-            <h1 className="text-xl font-semibold">User Information</h1>
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl font-semibold">User Information</h1>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button>Edit User</Button>
+                </SheetTrigger>
+                <EditUser />
+              </Sheet>
+            </div>
             <div className="space-y-4 mt-4">
               <div className="flex flex-col gap-2 mb-8">
                 <p className="text-sm text-muted-foreground">
---
+++ src/components/EditUser.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "./ui/sheet";
+
+const EditUser = () => {
+  return (
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle className="mb-4">Edit User</SheetTitle>
+        <SheetDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </SheetDescription>
+      </SheetHeader>
+    </SheetContent>
+  );
+};
+
+export default EditUser;
