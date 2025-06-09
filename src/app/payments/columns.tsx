20d76d47fd6fd4c9b3091ae98aa3e4e3d1c677eb
Anmol Shrestha <nmol.crestha@gmail.com>
2025-06-09 07:59:42 +0545

    Added datatable component

---
+++ src/app/payments/columns.tsx
@@ -0,0 +1,28 @@
+import { ColumnDef } from "@tanstack/react-table";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+  username: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+  {
+    accessorKey: "username",
+    header: "Name",
+  },
+];
